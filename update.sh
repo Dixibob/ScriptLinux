b69941832bbc52f15a388092bc3ee9667e9172f8
root <root@vps-dab6eef2.vps.ovh.net.novalocal>
2020-11-17 14:07:27 +0000

    Upload de update.sh

---
+++ update.sh
@@ -0,0 +1,5 @@
+echo "\n#############################UPDATE##############################"
+sudo apt-get update -y
+echo "\n#############################UPGRADE#############################"
+sudo apt-get upgrade -y
+echo "\n[OK]"
